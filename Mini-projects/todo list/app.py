ee117c9116e0904181d588144d8d5026be0b1bf4
Gokul <gokulnathg2003@outlook.com>
2026-05-30 14:57:28 +0530

    added view task functionality and invalid choice

--- Mini-projects/todo list/app.py
+++ Mini-projects/todo list/app.py
@@ -2,7 +2,8 @@ tasks = []
 
 while True:
     print("1. Add a task")
-    print("2. Exit")
+    print("2. View Tasks")
+    print("3. Exit")
 
     choice = input("Enter your choice: ")
 
@@ -10,6 +11,14 @@ while True:
         task = input("Enter the task: ")
         tasks.append(task)
         print("Task added successfully!")
-    elif choice == '2':
+
+    elif choice == "2":
+        for task in tasks:
+            print(task)
+
+    elif choice == '3':
         print("Exiting the program. Goodbye!")
         break
+
+    else:
+        print("Invalid Choice")
