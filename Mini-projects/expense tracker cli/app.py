781d40480607e90acc79d1da1c8e3cfe139e23d0
Gokul <gokulnathg2003@outlook.com>
2026-06-13 00:38:40 +0530

    v1 almost done with

--- Mini-projects/expense tracker cli/app.py
+++ Mini-projects/expense tracker cli/app.py
@@ -1,20 +1,62 @@
-expense= {}
+expenses= []
+
+def add_expense():
+        exp1 = input("enter expense name: ")
+        exp2 = int(input("enter expense amount: "))
+        exp3 = input("enter expense category: ")
 
 
+
+        new_expense = {
+            "expense_name": exp1,
+            "amount": exp2,
+            "category": exp3
+        }
+
+        expenses.append(new_expense)
+
+
+def view_expense():
+        if expenses:
+                
+            count = 1
+            for expense in expenses:
+                
+                print(count,".",
+                    expense["expense_name"],
+                        "|",
+                        expense["amount"],
+                        "|",
+                        expense["category"],
+                        )
+                count += 1
+        else:
+            print("no expenses tracked")
+
+def view_total():
+    count1 = 0
+    for expense in expenses:
+        sum = count1 + expense["amount"]
+        count1 = sum
+
+    print(f"total spending = {sum}")
 
 while True:
     print("*******************Expense Tracker************************")
-    print("1) Add Expense")
     print("1) Add Expense")
     print("2) View Expense")
     print("3) Delete Expense")
     print("4) Exit")
 
     choice = input("Enter your choice: ")   
-    if choice == '1':   
-        expense1 = input("enter expense")
-        
 
+    if choice == '1':   
+        add_expense()
+
+    
+    elif choice == '2':     
+        view_expense()
+        view_total()
 
 
     elif choice == '4':
@@ -23,4 +65,5 @@ while True:
 
     else:
         print("Invalid Choice") 
+
 
--- Mini-projects/expense tracker cli/test.py
+++ Mini-projects/expense tracker cli/test.py
@@ -19,7 +19,7 @@ expenses = [
         "expense_name": "Coffee",
         "amount": 120,
         "category": "Drink"
-    }
+    },
 
         {
         "expense_name": "Uber",
@@ -27,4 +27,36 @@ expenses = [
         "category": "Transport"
     }
 ]
+'''
+exp1 = input("enter expense name: ")
+exp2 = int(input("enter expense amount: "))
+exp3 = input("enter expense category: ")
 
+
+
+new_expense = {
+    "expense_name": exp1,
+    "amount": exp2,
+    "category": exp3
+}
+
+expenses.append(new_expense)
+'''
+count = 1
+for expense in expenses:
+    
+    print(count,".",
+           expense["expense_name"],
+             "|",
+             expense["amount"],
+             "|",
+             expense["category"],
+             )
+    count += 1
+
+count1 = 0
+for expense in expenses:
+    sum = count1 + expense["amount"]
+    count1 = sum
+
+print(f"total spending = {sum}")
