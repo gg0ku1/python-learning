d42da666798e86cc67a82413caa61f100bd07228
Gokul <gokulnathg2003@outlook.com>
2026-06-20 15:20:50 +0530

    contact book added view and delete function

--- Mini-projects/contact book/app.py
+++ Mini-projects/contact book/app.py
@@ -22,7 +22,25 @@ def add_contact():
     contact_email = input("enter name")
 
     new_contact = {"contact name":contact_name,
-                   "phone_number":phone_number,
-                   "contact_email":contact_email}
+                   "contact number":phone_number,
+                   "contact email":contact_email}
     
     contacts.append(new_contact)
+    print("contact added successfully")
+
+def view_contact():
+    if contacts:
+        count = 10
+        for contact in contacts:
+            print(f"{count}. {contact["contact name"]}|{contact["contact number"]}|{contact["contact email"]} ")
+            count+=1
+    else:
+        print("no contacts added")
+
+def delete_contact():
+    delete = int(input("enter contact to delete"))
+    if 0<delete<=len(contacts):
+        del contacts[delete-1]
+        print("contact deleted successfully")
+    else:
+        print("invalid entry")
