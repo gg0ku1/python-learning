c5249124bf4153d9891e84ada5d45323197d7846
Gokul <gokulnathg2003@outlook.com>
2026-06-21 17:53:01 +0530

    added file handling (save) in contact book

--- Mini-projects/contact book/app.py
+++ Mini-projects/contact book/app.py
@@ -3,10 +3,10 @@ contacts = []
 
 
 def add_contact():
-    print ("enter contact details: ")
-    contact_name = input("enter name")
-    phone_number = int(input("enter name"))
-    contact_email = input("enter name")
+    print ("enter contact details ")
+    contact_name = input("enter name: ")
+    phone_number = int(input("enter number: "))
+    contact_email = input("enter email: ")
 
     new_contact = {"contact name":contact_name,
                    "contact number":phone_number,
@@ -14,10 +14,18 @@ def add_contact():
     
     contacts.append(new_contact)
     print("contact added successfully")
+    save_contact()
+
+def save_contact():
+    with open("contacts.txt","w") as file:
+        for contact in contacts:
+            line = (f"{contact["contact name"]},{contact["contact number"]},{contact["contact email"]}")
+            file.write(line + "\n")
+
 
 def view_contact():
     if contacts:
-        count = 10
+        count = 1
         for contact in contacts:
             print(f"{count}. {contact["contact name"]}|{contact["contact number"]}|{contact["contact email"]} ")
             count+=1
@@ -25,15 +33,20 @@ def view_contact():
         print("no contacts added")
 
 def delete_contact():
-    delete = int(input("enter contact to delete"))
-    if 0<delete<=len(contacts):
-        del contacts[delete-1]
-        print("contact deleted successfully")
-    else:
-        print("invalid entry")
+    try:
+        delete = int(input("enter contact to delete: "))
+        if 0<delete<=len(contacts):
+            del contacts[delete-1]
+            print("contact deleted successfully")
+        else:
+            print("invalid entry")
+    except(ValueError):
+        print("please enter valid number")
+        
+    save_contact()
 
 def search_contact():
-    search = input("enter contact to search")
+    search = input("enter contact to search: ")
     found = False
     for contact in contacts:
        if contact["contact name"] == search:
@@ -54,7 +67,7 @@ while True:
     print("4. Delete Contact")
     print("5. Exit")
 
-    choice = input("Enter your choice")
+    choice = input("Enter your choice: ")
 
     if choice == "1":
         add_contact()
