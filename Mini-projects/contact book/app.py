32defa67345526cab24d33b3976390b571201bb9
Gokul <gokulnathg2003@outlook.com>
2026-06-21 17:29:32 +0530

    contact book added search and made functional

--- Mini-projects/contact book/app.py
+++ Mini-projects/contact book/app.py
@@ -1,19 +1,6 @@
 contacts = []
 
 
-while True:
-    print("*************CONTACT BOOK**************")
-
-    print("Enter choice")
-    print("1. Add Contact")
-    print("2. View Contact")
-    print("3. Search Contact")
-    print("4. Delete Contact")
-    print("5. Exit")
-
-    break
-
-
 
 def add_contact():
     print ("enter contact details: ")
@@ -44,3 +31,51 @@ def delete_contact():
         print("contact deleted successfully")
     else:
         print("invalid entry")
+
+def search_contact():
+    search = input("enter contact to search")
+    found = False
+    for contact in contacts:
+       if contact["contact name"] == search:
+           print(f'your contact is:{contact["contact name"]}|{contact["contact number"]}|{contact["contact email"]} ')
+           found = True
+
+    if found == False:
+        print("Contact not found")      
+
+
+while True:
+    print("*************CONTACT BOOK**************")
+
+    print("Enter choice")
+    print("1. Add Contact")
+    print("2. View Contact")
+    print("3. Search Contact")
+    print("4. Delete Contact")
+    print("5. Exit")
+
+    choice = input("Enter your choice")
+
+    if choice == "1":
+        add_contact()
+
+    elif choice == "2":
+        view_contact()
+
+    elif choice == "3":
+        search_contact() 
+
+    elif choice == "4":
+        delete_contact()
+    
+    elif choice == "5":
+        print("exiting app")
+        break
+
+    else:
+        print("invalid choice")
+
+
+
+
+    
