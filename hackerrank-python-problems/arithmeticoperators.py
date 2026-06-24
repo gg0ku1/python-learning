29ea62a19219a9449062ee64e41740b53c1673a3
Gokul <gokulnathg2003@outlook.com>
2026-06-24 14:02:21 +0530

    updated repo

---
+++ hackerrank-python-problems/arithmeticoperators.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+    
+summ = a+b
+diff = a-b
+prod = a*b
+
+print(summ)
+print(diff)
+print(prod)
---
+++ hackerrank-python-problems/loop.py
@@ -0,0 +1,8 @@
+n = int(input())
+    
+for i in range(n):
+    square = i ** 2
+    print(square)
+
+
+    
