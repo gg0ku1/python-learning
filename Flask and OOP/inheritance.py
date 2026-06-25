78ea70eef203e34778549a8440ec6949c74fdc24
Gokul <gokulnathg2003@outlook.com>
2026-06-25 13:57:38 +0530

    inheritance

---
+++ Flask and OOP/inheritance.py
@@ -0,0 +1,29 @@
+# Inheritance Allows a class to inherit attributes and methods from another class
+# Helps with code reusability and extensibility
+# class Child(Parent)
+
+
+class Animal:
+
+    def __init__(self, name):
+
+        self.name = name
+        self.is_alive = True
+
+    def eat(self):
+        print(f"{self.name} is eating")
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Dog(Animal):
+    pass
+
+class cat(Animal):
+    pass
+
+Husky = Dog("Scooby")
+Persian = cat("Garfield")
+
+print(Husky.name)
+
+Persian.sleep()
