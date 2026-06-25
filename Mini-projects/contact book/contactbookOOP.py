da2f48ac5bc6559d734834e8f545ae98dd2463b8
Gokul <gokulnathg2003@outlook.com>
2026-06-25 19:41:41 +0530

    contact book remade using OOP

---
+++ Mini-projects/contact book/contactbookOOP.py
@@ -0,0 +1,111 @@
+# import json
+
+class Contact:
+    def __init__(self, contact_name, phone_number, contact_email):
+        self.contact_name = contact_name
+        self.phone_number = phone_number
+        self.contact_email = contact_email
+
+    def display_data(self):
+        print(f" {self.contact_name}|{self.phone_number}|{self.contact_email} ")
+
+
+def add_contact():
+    print ("enter contact details ")
+    cn = input("enter name: ")
+    pn= input("enter number: ")
+    ce = input("enter email: ")
+
+    new_contact = Contact(cn, pn, ce)
+
+    contacts.append(new_contact)
+    print("contact added successfully")
+#     save_contact()
+
+# def save_contact():
+#     with open("contacts.json","w") as file:
+#         json.dump(contacts, file)
+
+
+def view_contact():
+    if contacts:
+        count = 1
+        for contact in contacts:
+            print(f"{count}.", end = "")
+            contact.display_data()
+            count+=1
+    else:
+        print("no contacts added")
+
+def delete_contact():
+    try:
+        delete = int(input("enter contact to delete: "))
+        if 0<delete<=len(contacts):
+            del contacts[delete-1]
+            print("contact deleted successfully")
+        else:
+            print("invalid entry")
+    except ValueError:
+        print("please enter valid number")
+
+    #  save_contact()
+
+def search_contact():
+    search = input("enter contact to search: ")
+    found = False
+    for contact in contacts:
+        if contact.contact_name == search:
+            print(f'your contact is:', end = "")
+            contact.display_data()
+            found = True
+
+    if found == False:
+        print("Contact not found")     
+
+contacts = []       
+
+# try:
+#     with open("contacts.json", "r") as file:
+#         contacts = json.load(file)
+
+# except FileNotFoundError:
+#     contacts = []
+
+ 
+
+
+while True:
+    print("*************CONTACT BOOK**************")
+
+    print("Enter choice")
+    print("1. Add Contact")
+    print("2. View Contacts")
+    print("3. Search Contact")
+    print("4. Delete Contact")
+    print("5. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        add_contact()
+
+    elif choice == "2":
+        view_contact()
+
+    elif choice == "3":
+        search_contact() 
+
+    elif choice == "4":
+        delete_contact()
+    
+    elif choice == "5":
+        print("exiting app")
+        break
+
+    else:
+        print("invalid choice")
+
+
+
+
+    
