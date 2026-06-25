de8f5bc6f9622d1d6a63a07250450c1190199d52
Gokul <gokulnathg2003@outlook.com>
2026-06-25 13:11:32 +0530

    class and object

---
+++ Flask and OOP/classandobject.py
@@ -0,0 +1,19 @@
+class student:
+
+    def __init__(self, name, major, year, marks):
+
+        self.name = name
+        self.major = major
+        self.year = year
+        self.marks = marks
+
+
+student1 = student("John","CS", "3rd year", 6.89)
+student2 = student("Jack","Mech", "3rd year", 7.2)
+
+
+print(student1.name)
+
+#above code shows how to type a basic student class, make instance and print
+
+
