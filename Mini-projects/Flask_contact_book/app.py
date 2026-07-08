a42eeaa0ceeb2db3716a7de2d34acb3079d98a5f
Gokul <gokulnathg2003@outlook.com>
2026-07-08 17:23:56 +0530

    v1 complete basic add and view

--- Mini-projects/Flask_contact_book/app.py
+++ Mini-projects/Flask_contact_book/app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+contacts = []
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/add", methods=["GET"])
+def add_page():
+    return render_template("add.html")
+
+@app.route("/add", methods=["POST"])
+def add():
+    name = request.form["name"]
+    phone = request.form["phone"]
+    email = request.form["email"]
+    new_contact = {"name":name, "phone":phone, "email":email}
+    contacts.append(new_contact)
+    print(contacts)
+    return redirect(url_for("view_page"))
+
+@app.route("/contacts")
+def view_page():
+    return render_template("contacts.html", contacts = contacts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
