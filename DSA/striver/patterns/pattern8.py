f8b305b56d14a54bf6803886ef6242f3fbdab40f
Gokul <gokulnathg2003@outlook.com>
2026-07-11 22:47:26 +0530

    pattern9

---
+++ DSA/striver/patterns/pattern8.py
@@ -0,0 +1,17 @@
+# *********
+#  *******
+#   *****
+#    ***
+#     *
+
+def pat8(n):
+    for row in range(n):
+        for col in range(row):
+            print(" ", end = "")
+        for col in range(2*n- (2*row+1)):
+            print("*",end = "" )
+         
+
+        print()
+
+pat8(5)
