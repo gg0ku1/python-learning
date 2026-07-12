cc6cff441428fdfe912dc1af40be1cd2efa9d20f
Gokul <gokulnathg2003@outlook.com>
2026-07-12 17:00:14 +0530

    pattern20

--- DSA/striver/patterns/pattern20.py
+++ DSA/striver/patterns/pattern20.py
@@ -1,32 +1,19 @@
-
-# **********
-# ****  ****
-# ***    ***
-# **      **
-# *        *
 # *        *
 # **      **
 # ***    ***
 # ****  ****
 # **********
+# ****  ****
+# ***    ***
+# **      **
+# *        *
 
 
 
-def pat19(n):
-    for row in range(n):
-        for col in range(n - row):
-            print("*", end = "")
-        for col in range(2*row):
-            print(" ",end = "" )
-       
-        for col in range(n-row):
-            print("*", end = "")
-        print()
-
+def pat20(n):
     for row in range(n):
         for col in range(row+1):
             print("*", end = "")
-
         for col in range(2 * (n - row - 1)):
             print(" ",end = "" )
        
@@ -34,5 +21,16 @@ def pat19(n):
             print("*", end = "")
         print()
 
+    for row in range(1,n):
+        for col in range(n-row):
+            print("*", end = "")
 
-pat19(5)
+        for col in range(2*row):
+            print(" ",end = "" )
+       
+        for col in range(n - row):
+            print("*", end = "")
+        print()
+
+
+pat20(5)
