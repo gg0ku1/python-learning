3c1fa5464fc6cbf60d82f7b38eab69eff3dc5ae2
Gokul <gokulnathg2003@outlook.com>
2026-07-14 15:28:07 +0530

    delete in flask cb

--- Mini-projects/Flask_contact_book/app.py
+++ Mini-projects/Flask_contact_book/app.py
@@ -20,11 +20,16 @@ def add():
     new_contact = {"name":name, "phone":phone, "email":email}
     contacts.append(new_contact)
     print(contacts)
-    return redirect(url_for("view_page"))
+    return redirect(url_for("home"))
 
 @app.route("/contacts")
 def view_page():
     return render_template("contacts.html", contacts = contacts)
+
+@app.route("/delete/<int:index>")
+def delete_page(index):
+    del contacts[index]
+    return redirect(url_for("view_page"))
 
 
 if __name__ == "__main__":
