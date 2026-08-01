5964165816d35e6f63d709c1a446f99a4ebaa591
Gokul <gokulnathg2003@outlook.com>
2026-08-01 10:57:08 +0530

    counting digits optimal

--- DSA/striver/Basic Maths/1countingdigits.py
+++ DSA/striver/Basic Maths/1countingdigits.py
@@ -1,3 +1,5 @@
+import math
+
 #brute force
 
 numb = 12345
@@ -11,6 +13,14 @@ print(counter)
 
 
 
+#optimal method (log)
+
+numb = 52809
+
+count = int(math.log10(numb)) + 1 
+
+print(count)
+
 
 
 
@@ -21,17 +31,16 @@ print(counter)
 # Time Complexity
 
 # How many times does the loop run?
-
 # For 12345, it runs 5 times.
-
 # For 987654321, it runs 9 times.
-
 # So it runs once per digit.
-
 # If the number has d digits:
-
 # Time: O(d)
 
 # You'll often see this written as:
-
 # O(log₁₀ N)
+
+
+# Optimal (Logarithm)
+# Time Complexity: O(1)
+# Space Complexity: O(1)
