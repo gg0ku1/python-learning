f81a762035fdafc18c53192f6fdd3a6f908f7b47
Gokul <gokulnathg2003@outlook.com>
2026-08-02 00:29:09 +0530

    printdivisors

--- DSA/striver/Basic Maths/5printalldivisors.py
+++ DSA/striver/Basic Maths/5printalldivisors.py
@@ -1,14 +1,35 @@
+import math
+
 #brute force approach
+
 
 n = 39
 
-divisors = []
 for i in range(1,n+1):
     if n % i == 0:
-        divisors.append(i)
+        print(i)
 
-print(divisors)
 
 
 # Time Complexity: O(N)
 # Space Complexity: O(number of divisors)
+
+#optimal approach
+
+n = 36
+
+divisors = []
+
+for i in range(1,int(math.sqrt(n)) + 1):
+    if n % i == 0:
+        divisors.append(i)
+        j = n//i
+        if i != j:
+            divisors.append(j)
+                
+divisors.sort()
+print(divisors)
+
+#Time: O(√N)
+
+
