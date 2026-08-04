d9150a7cfedf2b97d97f61fb682ec6ab5c91ce18
Gokul <gokulnathg2003@outlook.com>
2026-08-04 17:28:47 +0530

    Print Name N times using Recursion

---
+++ DSA/striver/Recursion/basicproblems.py
@@ -0,0 +1,16 @@
+# Print Name N times using Recursion
+
+def recur(n, i):
+    if i == n:
+        return
+
+    i += 1
+    print(name)
+    recur(n, i)
+
+
+n = int(input())   
+name = "giga" 
+
+recur(n, 0)
+
