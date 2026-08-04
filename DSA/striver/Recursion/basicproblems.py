ca4c8df7be5880d9348e8781330eac500a08b28c
Gokul <gokulnathg2003@outlook.com>
2026-08-04 17:53:57 +0530

    Print i to n using Recursion

--- DSA/striver/Recursion/basicproblems.py
+++ DSA/striver/Recursion/basicproblems.py
@@ -14,3 +14,34 @@ name = "giga"
 
 recur(n, 0)
 
+# Print 1 to N using Recursion
+
+def recur(n, i):
+    if i == n:
+        return
+
+    i += 1
+    print(i)
+    recur(n, i)
+
+
+n = int(input())   
+
+
+recur(n, 0)
+
+# Print N to 1 using Recursion
+
+# def recur(n, i):
+#     if i == 0:
+#         return
+
+#     print(i)
+#     i -= 1
+#     recur(i)
+
+
+# i = int(input())   
+
+
+# recur(i)
