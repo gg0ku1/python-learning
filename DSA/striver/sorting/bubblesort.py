f823b486b0e2d5c525f4b72636fdc5a8e2dfb3ba
Gokul <gokulnathg2003@outlook.com>
2026-08-25 21:27:46 +0530

    optimal bubble sort

--- DSA/striver/sorting/bubblesort.py
+++ DSA/striver/sorting/bubblesort.py
@@ -1,11 +1,16 @@
 
 arr = [3, 5, 1, 2, 4]
+
 n = len(arr)
 
 
 for i in range(n):
+    didswap = False
     for j in range(n-i-1):
         if arr[j] > arr[j+1]:
             arr[j], arr[j+1] = arr[j+1], arr[j]
+            didswap = True
+    if didswap == False:
+        break
 
 print(arr)
