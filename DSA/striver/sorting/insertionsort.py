7bdaf7829d234d04cfff10e70d3a08dd396302fd
Gokul <gokulnathg2003@outlook.com>
2026-08-29 23:53:06 +0530

    insertion sort

--- DSA/striver/sorting/insertionsort.py
+++ DSA/striver/sorting/insertionsort.py
@@ -3,7 +3,11 @@ n = len(arr)
 
 for i in range (1, n):
     key = arr[i]
-    for j in range(i-1):
-        if arr[j]>key:
-            pass
-#incomplete
+    j = i -1
+    while j >= 0 and arr[j]> key:
+        arr[j+1] = arr[j]
+        j -= 1
+
+    arr[j+1] = key
+
+print(arr)
