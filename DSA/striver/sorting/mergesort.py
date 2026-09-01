52cffcd5d29e43fa872a07dec224dded12e3a970
Gokul <gokulnathg2003@outlook.com>
2026-09-01 23:53:26 +0530

    manual merge sort

--- DSA/striver/sorting/mergesort.py
+++ DSA/striver/sorting/mergesort.py
@@ -6,3 +6,23 @@ mid = n // 2
 left = arr[:mid]
 right = arr[mid:] #slicing
 
+left_n = len(left)
+left_mid = left_n // 2
+
+left_l = left[:left_mid]
+left_r  = left[left_mid:]
+
+right_n = len(right)
+right_mid = right_n // 2
+
+right_l = right[:right_mid]
+right_r  = right[right_mid:]
+
+right_r_n = len(right_r)
+right_r_mid = right_r_n // 2
+
+right_r_l = right_r[:right_r_mid]
+right_r_r  = right_r[right_r_mid:]
+
+#base case reached
+
