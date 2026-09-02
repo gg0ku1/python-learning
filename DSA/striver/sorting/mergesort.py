e81cbdc1fe0a4933f2f56b935fd71987fa5faa57
Gokul <gokulnathg2003@outlook.com>
2026-09-02 20:25:54 +0530

    merge sort completed

--- DSA/striver/sorting/mergesort.py
+++ DSA/striver/sorting/mergesort.py
@@ -1,52 +1,42 @@
 arr = [5, 3, 4, 1, 2]
 
-n = len(arr)
-mid = n // 2
 
-left = arr[:mid]
-right = arr[mid:] #slicing
+def merge(left, right):
+    temp = []
+    i = 0
+    j = 0
 
-left_n = len(left)
-left_mid = left_n // 2
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            temp.append(left[i])
+            i += 1
 
-left_l = left[:left_mid]
-left_r  = left[left_mid:]
+        else:
+            temp.append(right[j])
+            j += 1
 
-right_n = len(right)
-right_mid = right_n // 2
-
-right_l = right[:right_mid]
-right_r  = right[right_mid:]
-
-right_r_n = len(right_r)
-right_r_mid = right_r_n // 2
-
-right_r_l = right_r[:right_r_mid]
-right_r_r  = right_r[right_r_mid:]
-
-#base case reached
-
-left = [3, 7, 10]
-right = [1, 5, 8]
-
-temp = []
-i = 0
-j = 0
-
-while i < len(left) and j < len(right):
-    if left[i] < right[j]:
+    while i < len(left):
         temp.append(left[i])
         i += 1
 
-    elif left[i] > right[j]:
+    while j < len(right):
         temp.append(right[j])
         j += 1
 
-while i < len(left):
-    temp.append(left[i])
-    i += 1
+    return temp
+    
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
 
-while j < len(right):
-    temp.append(right[j])
-    j += 1
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
 
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+sorted_arr = merge_sort(arr)
+print(sorted_arr)
