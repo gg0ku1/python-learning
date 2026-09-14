d9a1de7e0115b410581f2f9e51f7bc393fcc65b5
Gokul <gokulnathg2003@outlook.com>
2026-09-14 16:21:59 +0530

    api path parameters and specific resources

--- Flask/API practice/app.py
+++ Flask/API practice/app.py
@@ -2,13 +2,32 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+contacts = [
+        {
+            "id": 1,
+            "name": "John",
+            "phone": "9876543210"
+        },
+        {
+            "id": 2,
+            "name": "Sarah",
+            "phone": "9123456780"
+        }
+    ]
 
-@app.route("/api/hello")
-def hello():
-    return jsonify({
-        "message": "Hello from my API!"
-    })
 
+@app.route("/api/contacts")
+def get_contacts():
+
+    return jsonify(contacts)
+
+@app.route("/api/contacts/<int:id>")
+def get_contact(id):
+    for contact in contacts:
+        if contact["id"] == id:
+            return jsonify(contact)
+
+    return jsonify({"error": "Contact not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
