540b2fe31771ab50dfda864fb0892aaf61e10aa1
Gokul <gokulnathg2003@outlook.com>
2026-09-23 23:45:55 +0530

    rest api with database

--- Flask/API practice/dbapp.py
+++ Flask/API practice/dbapp.py
@@ -7,5 +7,12 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///contacts.db"
 
 db = SQLAlchemy(app)
 
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+    
